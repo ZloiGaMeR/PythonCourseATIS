d0ccca5580450a02e1e1ec6343c0e79e7b07ed3b
IlyaOrlov <orlov.soft.company@gmail.com>
2021-12-23 14:16:54 +0300

    For lec2

--- Useful/for_lec2/draw_lines.py
+++ Useful/for_lec2/draw_lines.py
@@ -8,17 +8,17 @@ canvas.pack()
 img = PhotoImage(width=WIDTH, height=HEIGHT)
 canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
 
+# некорректно
+# img.put("#ffffff", (100, 100))
+# img.put("#ffffff", (101, 100))
+# img.put("#ffffff", (102, 100))
+# img.put("#ffffff", (103, 100))
+# img.put("#ffffff", (104, 100))
 
-img.put("#ffffff", (100, 100))
-img.put("#ffffff", (101, 100))
-img.put("#ffffff", (102, 100))
-img.put("#ffffff", (103, 100))
-
-# x = 100
-# y = 100
-# while y < 480:
-#     img.put("#ffffff", (x, y))
-#     y += 1
-
-
+# корректно
+x = 100
+y = 100
+while x < WIDTH:
+    img.put("#ffffff", (x, y))
+    x += 1
 mainloop()
