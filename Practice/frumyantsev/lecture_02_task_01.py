86a5e40239d516d757ae376b6a744f3a25420d9e
unknown <fruittrainings@gmail.com>
2022-01-02 00:20:05 +0300

    Second attempt of pushing files.

---
+++ Practice/frumyantsev/lecture_02_task_01.py
@@ -0,0 +1,4 @@
+max_value = lambda x = input("Key in value1: "), y = input("Key in value2: "): print(max(x,y))
+
+max_value()
+#
---
+++ Practice/frumyantsev/lecture_02_task_02.py
@@ -0,0 +1,5 @@
+max_value = lambda x = input("Key in value1: "), y = input("Key in value2: "): max(x,y)
+
+max_value_returned = max_value()
+print(f"Function \"max_value\" returns: {max_value_returned}") # проверка
+#
