8eb88028d90f43d0ebdcc054df9a0ac0fe451191
Olga Bugrova <sol.reykan@gmail.com>
2022-01-09 01:07:36 +0300

    Лекция 2, задания 1, 2

---
+++ Practice/obugrova/lec2_ex.1,2.py
@@ -0,0 +1,14 @@
+def biggest (a, b):
+    if a > b:
+        print (a)
+    else:
+        print (b)
+biggest (2, 3)
+
+def returnbiggest (a, b):
+    if a > b:
+        return a
+    else:
+        return b
+c = returnbiggest(8, 7)
+print(c)
