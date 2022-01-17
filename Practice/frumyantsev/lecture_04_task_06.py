6d18d1e7f1c09a892346b33ee3c4115b2a25ee72
unknown <fruittrainings@gmail.com>
2022-01-17 11:21:14 +0300

    Исправил lecture_04_task_06.py.

--- Practice/frumyantsev/lecture_04_task_06.py
+++ Practice/frumyantsev/lecture_04_task_06.py
@@ -16,12 +16,12 @@ lst3 = [[1, 4, 5, 0],
 
 def remove_column(numb, lst):
 
-    columns_to_remove = []
+    columns_to_remove = set()
     for item in range(len(lst)):
         for subitem in range(len(lst[item])):
             if lst[item][subitem] == numb:
-                columns_to_remove.append(subitem)
-    columns_to_remove = list(set(columns_to_remove))
+                columns_to_remove.add(subitem)
+    columns_to_remove = list(columns_to_remove)
     columns_to_remove.sort(reverse=True)
 
     for col in columns_to_remove:
@@ -32,10 +32,10 @@ def remove_column(numb, lst):
 
 
 if __name__ == "__main__":
-    # remove_column(7, lst0)
-    # remove_column(7, lst1)
+    remove_column(7, lst0)
+    remove_column(7, lst1)
     # remove_column(2, lst1)
     # remove_column(3, lst2)
-    # remove_column(5, lst2)
+    remove_column(5, lst2)
     # remove_column(6, lst2)
     remove_column(6, lst3)
