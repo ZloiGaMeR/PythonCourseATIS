4801f1d2f74c0b6dbb693f15f805a8a87feb6e16
IlyaOrlov <orlov.soft.company@gmail.com>
2022-01-18 23:31:50 +0300

    Examples for lec5

--- Useful/for_lec5/inheritance_example.py
+++ Useful/for_lec5/inheritance_example.py
@@ -1,3 +1,5 @@
+# Sport club
+
 class Position:
     def __init__(self, money):
         self.money = money
@@ -19,25 +21,25 @@ class Employee:
         return f"Employee {self._name} {self._surname} with {self.__free_time}"
 
 
-class ExampleBase:
-    def __init__(self, attr, *args, **kwargs):
+class Sportsman:
+    def __init__(self, team, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._attr = attr
+        self._team = team
 
 
-class Player(ExampleBase):
-    def __init__(self, attr, sport, *args, **kwargs):
-        super().__init__(attr, *args, **kwargs)
-        self._sport = sport
+class Footballer(Sportsman):
+    def __init__(self, team, league, *args, **kwargs):
+        super().__init__(team, *args, **kwargs)
+        self._league = league
 
 
-class Manager(Employee, Player):
+class Manager(Employee, Footballer):
     def __init__(self, name, surname, salary, employees=tuple()):
-        super().__init__(name, surname, salary, 'attr', 'football')
+        super().__init__(name, surname, salary, 'Spartak', 'Premier league')
         # Альтернативный вызов конструкторов, если в родительских
         # классах не предусмотрены вызовы super().__init__(*args, **kwargs):
         # super().__init__(name, surname, salary)
-        # super(Employee, self).__init__('football')
+        # super(Employee, self).__init__('Premier league')
         self._employees = employees
 
     def __repr__(self):
@@ -46,5 +48,5 @@ class Manager(Employee, Player):
 
 e1 = Employee("Ivan", "Ivanov", 50000)
 m = Manager("Petr", "Petrov", 100000, (e1,))
-print(m._sport)
-print(m._attr)
+print(m._league)
+print(m._team)
