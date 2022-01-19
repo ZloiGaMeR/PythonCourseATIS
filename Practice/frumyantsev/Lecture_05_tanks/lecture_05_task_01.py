dfc54034d62f7a684ac9d774ec2e20213e148e86
Fedor <fruittrainings@gmail.com>
2022-01-19 15:41:34 +0300

    Спасибо. Исправил первый пример из разминки.

---
+++ Practice/frumyantsev/Lecture_05_tanks/lecture_05_task_01.py
@@ -0,0 +1,14 @@
+class Man:
+    name = ''
+
+    def __init__(self):
+        self.name = input("Введите имя: ")
+
+    # @staticmethod
+    def solve_task(self):
+        print("I'm not ready yet")
+
+
+m1 = Man()
+print(m1.name)
+m1.solve_task()
