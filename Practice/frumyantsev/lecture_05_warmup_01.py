d0e94684b20ee0c6944ddf8856dce50e4feda55f
Fedor <fruittrainings@gmail.com>
2022-01-19 15:39:56 +0300

    Спасибо. Исправил первый пример из разминки.

--- Practice/frumyantsev/lecture_05_warmup_01.py
+++ Practice/frumyantsev/lecture_05_warmup_01.py
@@ -3,13 +3,18 @@ def mymax(arguments):
     Принимает любое количество аргументов и возвращает максимальный аргумент:
     """
     max_val = float("-inf")
-    for i in range(len(arguments)):
-        if arguments[i] > max_val:
-            max_val = arguments[i]
+    for i in arguments:
+        if i > max_val:
+            max_val = i
     print(max_val)
+    # for i in range(len(arguments)):
+    #     if arguments[i] > max_val:
+    #         max_val = arguments[i]
+    # print(max_val)
     return max_val
 
 
 if __name__ == "__main__":
     mymax([10, 27, 42, 36])
     mymax((3, 7, -1, -8, 0))
+    mymax({-5, 8, 0, -6, 12, 3})
