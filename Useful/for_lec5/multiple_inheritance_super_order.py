3372e96a351e906daec090a0767773f558630d12
IlyaOrlov <orlov.soft.company@gmail.com>
2022-01-19 22:13:46 +0300

    Examples for lec5

---
+++ Useful/for_lec5/multiple_inheritance_super_order.py
@@ -0,0 +1,31 @@
+class Parent1:
+    def __init__(self, *args, **kwargs):
+        print(f"Parent1")
+        super().__init__(*args, **kwargs)
+
+
+class Child1(Parent1):
+    def __init__(self, *args, **kwargs):
+        print(f"Child1")
+        super().__init__(*args, **kwargs)
+
+
+class Parent2:
+    def __init__(self, *args, **kwargs):
+        print(f"Parent2")
+        super().__init__(*args, **kwargs)
+
+
+class Child2(Parent2):
+    def __init__(self, *args, **kwargs):
+        print(f"Child2")
+        super().__init__(*args, **kwargs)
+
+
+class MyClass(Child1, Child2):
+    def __init__(self, *args, **kwargs):
+        print(f"MyClass")
+        super().__init__(*args, **kwargs)
+
+
+m = MyClass()
