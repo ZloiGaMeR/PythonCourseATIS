475dc7c17aa02bdc350283131d1e70b05710a57c
ismartynenko <martein87@gmail.com>
2022-01-27 22:12:08 +0300

    Убрал self у аттрибута filepath. и объявил его как global
    Скритп работает, но есть предупреждение:
    "Global variable 'filepath' is undefined at the module level"

--- Practice/ismartynenko/practice5_3_wrap.py
+++ Practice/ismartynenko/practice5_3_wrap.py
@@ -5,14 +5,15 @@ from tempfile import mktemp
 class WrapStrToFile:
     def __init__(self): 
         # здесь инициализируется атрибут filepath, он содержит путь до файла-хранилища
-        self.filepath = mktemp()
+        global filepath
+        filepath = mktemp()
 
     @property 
     def content(self): 
         # попытка чтения из файла, в случае успеха возвращаем содержимое 
         # в случае неудачи возвращаем 'File doesn't exist'
         try:
-            with open(self.filepath, "r") as fp:
+            with open(filepath, "r") as fp:
                 text = fp.read()
         except FileNotFoundError:
             print("File doesn't exist")
@@ -22,13 +23,13 @@ class WrapStrToFile:
     @content.setter
     def content(self, value): 
         # попытка записи в файл указанного содержимого
-        with open(self.filepath, "w+") as fp:
+        with open(filepath, "w") as fp:
             fp.write(value)
 
     @content.deleter 
     def content(self): 
         # удаляем файл: os.remove(имя_файла)
-        os.remove(self.filepath)
+        os.remove(filepath)
 
 
 wstf = WrapStrToFile()
