8df46840bd1f74174542a85b9834dd05d735e531
ismartynenko <martein87@gmail.com>
2022-02-09 16:58:05 +0300

    №6 - добавил пре-компиляцию регулярных выражений

--- Practice/ismartynenko/exam_6.py
+++ Practice/ismartynenko/exam_6.py
@@ -2,18 +2,21 @@ import re
 
 
 def myformat(mystr, *args):
-    obj = re.findall(r'{\d}', mystr)
+    regexp_c1 = re.compile(r'{\d}')
+    regexp_c2 = re.compile(r'{}')
+
+    obj = re.findall(regexp_c1, mystr)
     if len(obj) != 0:
         arr = re.findall(r'\d', mystr)
         for i in range(len(obj)):
-            mystr = re.sub(r"{\d}", args[int(arr[i])], mystr, 1)
-        print(mystr)
+            mystr = re.sub(regexp_c1, args[int(arr[i])], mystr, 1)
+        return mystr
     else:
-        obj = re.findall('{}', mystr)
+        obj = re.findall(regexp_c2, mystr)
         for i in range(len(obj)):
-            mystr = re.sub("{}", args[i], mystr, 1)
-        print(mystr)
+            mystr = re.sub(regexp_c2, args[i], mystr, 1)
+        return mystr
 
 
-myformat('{1}, {0}, {2}', 'a', 'b', 'c')
-myformat('coordinates: {}, {}', '37.4N', '18.3W')
+print(myformat('{1}, {0}, {2}', 'a', 'b', 'c'))
+print(myformat('coordinates: {}, {}', '37.4N', '18.3W'))
