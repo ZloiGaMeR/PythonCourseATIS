c58b4623bbb5e296969d01fe0408123b99831719
ismartynenko <martein87@gmail.com>
2022-02-11 20:50:57 +0300

    Лекция 7 задание 3, FIX

--- Practice/ismartynenko/practice7_3.py
+++ Practice/ismartynenko/practice7_3.py
@@ -6,7 +6,6 @@ name_list = ["Peter", "John", "Travis", "Bob", "Alex", "Rachel", "Michael", "Sin
 surname_list = ["Cooper", "Butler", "McAlester", "Rostenkowsky", "Kutrapaly", "Dexter", "Richi", "Tores"]
 fname_list = ["Sergeevich", "Popovich", "Alekseevich", "Vyacheslavovich", "Aleksandrovich", "Vladimirovich"]
 car_list = ["Tesla", "Volvo", "VW", "Audi", "Kia", "Nissan", "Renault", "Lada", "Chevrolet", "Suzuki", "Honda"]
-people_lst = []
 
 
 class Human:
@@ -20,29 +19,22 @@ class Human:
 
 
 def create(num):
+    people_lst = []
     with open("human.data", "wb") as f:
         for i in range(num):
-
             name = choice(name_list)
             surname = choice(surname_list)
             fname = choice(fname_list)
             age = randint(18, 65)
             postal = '603' + str(randint(100, 200))
             car = choice(car_list)
-
             people_lst.append(Human(name, surname, fname, age, postal, car))
-            pickle.dump(people_lst[i].__dict__, f)
+        pickle.dump(people_lst, f)
 
 
 def read():
-    # тут не смог реализовать следующее решение
-    # res = pickle.load(f)
-    # return res
-    # считывало только первую строку
-    unsrl = []
     with open("human.data", "rb") as f:
-        for _ in people_lst:
-            unsrl.append(pickle.load(f))
+        unsrl = pickle.load(f)
     return unsrl
 
 
