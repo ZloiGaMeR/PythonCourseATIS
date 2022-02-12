97bdf9af63ed27ac8bdba9182fd96d40f84a72f3
Fedor <fruittrainings@gmail.com>
2022-02-13 02:12:49 +0300

    Задание 5 из домашней работы по лекции 7.

--- Practice/frumyantsev/Lecture_07/lecture_07_task_05.py
+++ Practice/frumyantsev/Lecture_07/lecture_07_task_05.py
@@ -9,8 +9,16 @@ pth = os.path.join(os.path.abspath(os.curdir), 'README.md')
 fle = open(pth, 'rb')
 by = fle.read()
 st = by.decode(encoding='UTF-8')
-res = re.findall(r'git [a-z]+[\s]*[a-zA-Z/.:@]+', st)
 
-for rs in res:
+res1 = re.findall(r' git [a-z][a-zA-Z\./:@\s_-]+[\" ]?' + r'\".*\"', st)
+res2 = re.findall(r'\"git [a-z][a-zA-Z\./:@\s_-]+\"', st)
+res3 = re.findall(r' git [a-z][a-zA-Z\./:@\s_-]+[^\r\n\d,\",\),\(,а-яА-Я,\s-]', st)
+
+for rs in res1:
     print(f"{rs}")
+for rs in res2:
+    print(f"{rs}")
+for rs in res3:
+    print(f"{rs}")
+
 
