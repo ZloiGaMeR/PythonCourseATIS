b13fc919eb77874c10c8553019a2d096fb8c8f7d
ismartynenko <martein87@gmail.com>
2022-02-12 22:55:23 +0300

    Экзамен задача 8 - Optimize

--- Practice/ismartynenko/exam_8.py
+++ Practice/ismartynenko/exam_8.py
@@ -3,11 +3,7 @@ import os.path
 
 
 def copydir(src, dst):
-    dir_and_files = []
     for i in os.walk(src):
-        dir_and_files.append(i)
-
-    for i in dir_and_files:
         path = str(i[0]).replace(os.path.dirname(src), dst)
         os.mkdir(path)
         for j in i[2]:
