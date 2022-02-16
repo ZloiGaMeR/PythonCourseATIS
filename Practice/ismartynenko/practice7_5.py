de6cf44cac1f87cfd4404bfc508c8fc143682c60
ismartynenko <martein87@gmail.com>
2022-02-16 16:11:32 +0300

    Практика 7-5 - Исправление

--- Practice/ismartynenko/practice7_5.py
+++ Practice/ismartynenko/practice7_5.py
@@ -3,15 +3,14 @@ import re
 with open("../README.md", "r", encoding="utf-8") as f:
     text = f.readlines()
     regexp = []
+    after_clean = []
     for i in text:
         matchObj = re.findall(r'git [a-z]+ ?[-_:/.@A-z]* ?[_a-z]*[ "А-я]*', i)
         if len(matchObj) != 0:
-            regexp += matchObj
+            for match in matchObj:
+                after_clean = match.rstrip()
+                if after_clean.count('"') == 1 and after_clean.endswith('"'):
+                    after_clean = match.rstrip('"')
+            regexp.append(after_clean)
 
-clean_lst = []
-for i in regexp:
-    if i.count('"') == 1 and i.endswith('"'):
-        clean_lst.append(i.rstrip('"'))
-    else:
-        clean_lst.append(i.rstrip())
-print(clean_lst)
+print(regexp)
