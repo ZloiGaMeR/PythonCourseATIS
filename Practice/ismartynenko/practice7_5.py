b04fd127646a0395123af701159d52a0b3a4c0a7
ismartynenko <martein87@gmail.com>
2022-02-16 12:01:25 +0300

    Практика 7-5 - поправил + подчистил лишние ковычки и пробелы, обычными строковыми методами

--- Practice/ismartynenko/practice7_5.py
+++ Practice/ismartynenko/practice7_5.py
@@ -6,5 +6,12 @@ with open("../README.md", "r", encoding="utf-8") as f:
     for i in text:
         matchObj = re.findall(r'git [a-z]+ ?[-_:/.@A-z]* ?[_a-z]*[ "А-я]*', i)
         if len(matchObj) != 0:
-            regexp.append(matchObj)
-    print(regexp)
+            regexp += matchObj
+
+clean_lst = []
+for i in regexp:
+    if i.count('"') == 1 and i.endswith('"'):
+        clean_lst.append(i.rstrip('"'))
+    else:
+        clean_lst.append(i.rstrip())
+print(clean_lst)
