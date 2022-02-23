3af0c0c9baa7cbe5faff0a578ddef2dadd38f4bd
ismartynenko <martein87@gmail.com>
2022-02-23 22:10:58 +0300

    Практика 8-1 - переделал

--- Practice/ismartynenko/practice8_1.py
+++ Practice/ismartynenko/practice8_1.py
@@ -1,27 +1,27 @@
 def sorted_list(a):
-    d = {}
-    d_sort = []
+    # Sort list
+    # Тут я думал как мне не создавать новый список, но никак не выходило.
+    # хотел что то вроде: a.sort(key=lambda x: a.count(x)). Ну ладно,
+    # работаем с копией, но тут тогда другой момент
+    # почему не могу сделать так: lst_sort.sort(key=lambda x: lst_sort.count(x)) ???
+    lst_sort = a.copy()
+    lst_sort.sort(key=lambda x: a.count(x))
 
     # Create dictionary - d[element] = num
-    for i in a:
-        d[i] = a.count(i)
+    d = {}
+    for i in lst_sort:
+        d[i] = lst_sort.count(i)
 
-    # Sorting
-    while len(d) != 0:
-        mini_val = d[lst[0]]
-        mini_key = lst[0]
-        for i in d.items():
-            if i[1] < mini_val:
-                mini_val = i[1]
-                mini_key = i[0]
+    # Create list
+    lst_final = []
+    for i in d.keys():
+        lst_final.append(i)
 
-        # Add to sorted list and delete from unsorted dictionary
-        d_sort.append(mini_key)
-        del d[mini_key]
-    print(d_sort)
+    return lst_final
 
 
-lst = [r'5_wefwr', '3_dd', '1_w', '5_wefwr', '2_23', '3_dd', '4', '5_wefwr',
-       '5_wefwr', '5_wefwr', '2_23', '3_dd', '4', '4', '4', 'unic']
+lst = [r'wefwr', '3_dd', '1_w', 'wefwr', '2_23', '3_dd', '4', 'wefwr',
+       'wefwr', 'wefwr', '2_23', '3_dd', '4', '4', '4', 'unic']
 
-sorted_list(lst)
+fun = sorted_list(lst)
+print(fun)
