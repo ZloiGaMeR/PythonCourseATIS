c7e448e34e2a0686d611a6d03fc6ff5f7ddb3d21
ZloiGaMeR <dr_tom@mail.ru>
2022-02-28 10:28:50 +0300

    9_2

---
+++ Practice/a.belyantsev/Lec9_2.py
@@ -0,0 +1,14 @@
+# Реализовать запуск функции, осуществляющей операцию сложения для различных типов (integer, string, list)
+# параллельно с различными наборами аргументов.
+from multiprocessing import Pool
+
+
+def summ(a, b):
+    return a+b
+
+
+if __name__ == '__main__':
+    with Pool(processes=3) as p:
+        res = p.starmap(summ, [(1, 2), ("1", "2"), ([1, 2, 3], [4, 5])])
+    for _ in res:
+        print(_)
